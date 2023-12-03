ca94bfe65abc8aa2e87fa5ff4551990b0f373cb5
HurdyGutty <92365356+HurdyGutty@users.noreply.github.com>
2023-12-03 14:06:48 +0700

    add and read text

---
+++ typescript/day_1/text.ts
@@ -0,0 +1,10 @@
+import * as fs from 'node:fs/promises';
+
+export async function readInput(): Promise<string> {
+    return await fs.readFile('day_1/day_1.txt', { encoding: 'utf-8', flag: 'r' })
+}
+
+
+export function parseInput(input: string): string[] {
+    return input.split('\r\n');
+}
