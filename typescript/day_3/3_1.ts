06774581eef30ad113d19bdff8d6211e3bbcad5c
HurdyGutty <92365356+HurdyGutty@users.noreply.github.com>
2023-12-04 08:23:27 +0700

    day 3 problem 1

---
+++ typescript/day_3/3_1.ts
@@ -0,0 +1,60 @@
+import { parseInput, readInput } from "./text.js";
+
+let input = await readInput();
+let lines: string[] = parseInput(input);
+
+const specialChars = ['!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '-', '+', '=', '/', '\\'];
+
+
+
+function isSpecial(char: string){
+    return specialChars.includes(char);
+}
+
+function isNumber(char: string){
+    return !isNaN(parseInt(char));
+}
+let sum_row = 0;
+
+for (let row = 0; row < lines.length; row++){
+    let line = lines[row];
+    let flag = false;
+    let number = 0;
+    for (let i = 0; i < line.length; i++){
+        
+        if (isNumber(line[i])){
+            number += parseInt(line[i]);
+            if (isNumber(line[i + 1])) {
+                number *= 10;
+            }
+            if (isSpecial(line[i + 1]) || isSpecial(line[i - 1])){
+                flag = true;
+            }
+            
+            if (row > 0){
+                if (isSpecial(lines[row - 1][i - 1]) || isSpecial(lines[row - 1][i]) || isSpecial(lines[row - 1][i + 1])){
+                    flag = true;
+                }
+            }
+            if (row < lines.length - 1){
+                if (isSpecial(lines[row + 1][i - 1]) || isSpecial(lines[row + 1][i]) || isSpecial(lines[row + 1][i + 1])){
+                    flag = true;
+                }
+            }
+            console.log(row, number, flag);
+        } else {
+                if (flag === true){
+                    sum_row += number;
+                    flag = false;
+                }
+                number = 0;
+            }
+        if (i == line.length - 1 && flag === true){
+            sum_row += number;
+            flag = false;
+        }
+    }
+}
+
+
+console.log(sum_row);
